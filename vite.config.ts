f3c2ad26df54a9121e9fd904494937d674d9f3ed
kwakseongjae <gkffhdnls13@gmail.com>
2024-02-15 00:11:51 +0900

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@api', replacement: path.resolve(__dirname, 'src/api') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@store', replacement: path.resolve(__dirname, 'src/store') },
+      { find: '@style', replacement: path.resolve(__dirname, 'src/style') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/util') },
+    ],
+  },
+});
