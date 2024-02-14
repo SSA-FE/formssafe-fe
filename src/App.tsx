3e37878787d6aee230b980b51c5f3afdb607462d
kwakseongjae <gkffhdnls13@gmail.com>
2024-02-15 08:47:02 +0900

    chore: react-router-dom 의존성 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<></>} />
+    </Routes>
+  );
 }
 
 export default App;
