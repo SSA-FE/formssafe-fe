304c614b7027e0e85761f1eeb1fb6f1785b7e602
KimYuKyung <kyg0954@naver.com>
2024-02-23 17:55:50 +0900

    feat: modal 컴포넌트 구현

---
+++ src/components/modal.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+interface IModal {
+  maxWidth: string;
+  state: boolean;
+  children?: React.ReactNode;
+}
+
+const Modal = ({ maxWidth, state, children }: IModal) => {
+  useEffect(() => {
+    if (!state) return;
+    document.body.style.cssText = `
+      position: fixed;
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, [state]);
+
+  return (
+    <>
+      {state &&
+        createPortal(
+          <div className="flex justify-center items-center w-full h-full bg-black bg-opacity-25 top-0 left-0 fixed">
+            <div
+              className={`flex flex-col w-full ${maxWidth} bg-white rounded-lg px-8 pt-8 pb-16 gap-y-8`}
+            >
+              {children}
+            </div>
+          </div>,
+          document.body
+        )}
+    </>
+  );
+};
+
+export default Modal;
