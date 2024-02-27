08ea395cd76ae7ac99e0c8d599553c5030f1a91f
kwakseongjae <gkffhdnls13@gmail.com>
2024-02-27 14:27:03 +0900

    feat: errorElement 컴포넌트 추가

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -1,10 +1,12 @@
 import Home from '@/pages/Home';
+import NotFound from '@/pages/NotFound';
 import Workspace from '@/pages/Workspace';
 
 export const RouterInfo = [
   {
     path: '/',
     element: <Home />,
+    errorElement: <NotFound />,
   },
   {
     path: '/main',
