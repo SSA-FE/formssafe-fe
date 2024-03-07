5b9e6bf93a0f1a2fa15341c7f914cc43be3cc068
hanna kang <hanna0175@gmail.com>
2024-03-08 03:35:17 +0900

    feat: 에디터 페이지 레이아웃 및 툴바 UI 작업

---
+++ src/components/Workspace/FormToolBar.tsx
@@ -0,0 +1,28 @@
+import SearchBar from '@/components/Workspace/SearchBar';
+import visibleIcon from '@/assets/icons/visible-icon.svg';
+
+const FormToolBar = () => {
+  return (
+    <div className="flex w-full px-8 h-[3.25rem] min-h-[3.25rem] whitespace-nowrap justify-between items-start border-b-neutral-200 border-b">
+      <div className="flex gap-md">
+        <button className="box-border px-6 py-2 text-sm text-white rounded bg-neutral-800">
+          지금 발행하기
+        </button>
+        <button className="flex items-center py-2 pl-4 pr-6 text-sm rounded gap-md bg-neutral-300">
+          <img src={visibleIcon} alt="미리보기 아이콘" />
+          미리보기
+        </button>
+      </div>
+      <div className="flex flex-row items-center text-xs select-none gap-md text-neutral-400">
+        <span>version-2024</span>
+        <SearchBar
+          placeholder="질문을 검색하세요"
+          bgColor="bg-neutral-100"
+          width="w-[107px]"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FormToolBar;
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,13 +1,17 @@
 import FormInfoBar from '@/components/Workspace/FormInfoBar';
 import FormMain from '@/components/Workspace/FormMain';
 import FormOptionBar from '@/components/Workspace/FormOptionBar';
+import FormToolBar from '@/components/Workspace/FormToolBar';
 
 const Editor = () => {
   return (
-    <div className="flex">
-      <FormInfoBar />
-      <FormMain />
-      <FormOptionBar />
+    <div>
+      <FormToolBar />
+      <div className="flex pt-2 bg-neutral-50">
+        <FormInfoBar />
+        <FormMain />
+        <FormOptionBar />
+      </div>
     </div>
   );
 };
