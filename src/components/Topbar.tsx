5d7748acab06dbd9f2971a17500a5d72eb78f0cd
kwakseongjae <gkffhdnls13@gmail.com>
2024-03-20 16:48:24 +0900

    style: topbar 스타일에서  border-b 제거

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,27 +1,27 @@
 const Topbar = () => {
   return (
-    <nav className="w-full h-topbar min-h-16 flex items-center justify-between border-b  border-neutral-300 px-8 py-2 whitespace-nowrap overflow-clip">
-      <div className="flex gap-9 align-middle">
-        <p className="text-slate-800 text-base font-bold">폼나는 싸패</p>
+    <nav className="flex items-center justify-between w-full px-8 py-2 h-topbar min-h-16 border-neutral-300 whitespace-nowrap overflow-clip">
+      <div className="flex align-middle gap-9">
+        <p className="text-base font-bold text-slate-800">폼나는 싸패</p>
         <div className="flex items-center gap-4 text-sm">
-          <span className="flex items-center text-neutral-500 rounded-2xl px-4 h-full bg-neutral-200 ">
+          <span className="flex items-center h-full px-4 text-neutral-500 rounded-2xl bg-neutral-200 ">
             v 3.4.1 24/02/13
           </span>
-          <span className="flex items-center bg-primaryTag text-primaryTag rounded-2xl px-4 h-full">
+          <span className="flex items-center h-full px-4 bg-primaryTag text-primaryTag rounded-2xl">
             2024 SAFFE FORM PROJECT Create your own Form
           </span>
         </div>
       </div>
 
-      <ul className="flex items-center h-full text-neutral-500 font-normal text-sm gap-4">
+      <ul className="flex items-center h-full gap-4 text-sm font-normal text-neutral-500">
         <li className="flex items-center h-full px-8 cursor-pointer">
           <p>리포지토리</p>
         </li>
-        <li className="h-full flex justify-center items-center px-8 cursor-pointer">
+        <li className="flex items-center justify-center h-full px-8 cursor-pointer">
           <p className="w-100">구성원 소개</p>
         </li>
         <li className="h-full">
-          <button className="h-full px-8 bg-black text-white rounded-lg font-bold text-sm">
+          <button className="h-full px-8 text-sm font-bold text-white bg-black rounded-lg">
             바로 작성하기
           </button>
         </li>
