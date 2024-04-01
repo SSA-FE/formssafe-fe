8cf7341411bab7afa1b47d5a978f680591062a64
kwakseongjae <gkffhdnls13@gmail.com>
2024-04-01 17:32:39 +0900

    add: Add userSlice and configure store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,11 @@
-/**
- * This is the main store configuration file.
- */
 import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from '@components/users/userSlice';
 
-export default configureStore({
-  reducer: {},
+export const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
