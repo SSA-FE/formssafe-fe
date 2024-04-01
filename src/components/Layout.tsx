a4eb61be3de4f49463536442986535394a2fdd92
kwakseongjae <gkffhdnls13@gmail.com>
2024-04-02 00:06:52 +0900

    feat: useFetchUsers 훅 추가 및 Layout 컴포넌트에 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,8 +2,14 @@ import { Outlet } from 'react-router-dom';
 import Topbar from '@/components/Topbar';
 import { Suspense } from 'react';
 import Loading from './Loading';
+import { useFetchUsers } from '@/hooks/useFetchUsers';
 
 export default function Layout() {
+  const { loading, error } = useFetchUsers();
+
+  if (loading) return <Loading />;
+  if (error) return <div>{error}</div>;
+
   return (
     <Suspense fallback={<Loading />}>
       <Topbar />
