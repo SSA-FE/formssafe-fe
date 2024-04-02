f3ed75496926b128b424e16325fa38ff745c35ce
kwakseongjae <gkffhdnls13@gmail.com>
2024-04-02 23:57:36 +0900

    fix: 무한 로딩이슈 임시 해결(향후 수정)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,14 +2,8 @@ import { Outlet } from 'react-router-dom';
 import Topbar from '@/components/Topbar';
 import { Suspense } from 'react';
 import Loading from './Loading';
-import { useFetchUsers } from '@/hooks/useFetchUsers';
 
 export default function Layout() {
-  const { loading, error } = useFetchUsers();
-
-  if (loading) return <Loading />;
-  if (error) return <div>{error}</div>;
-
   return (
     <Suspense fallback={<Loading />}>
       <Topbar />
--- src/hooks/useFetchUsers.tsx
+++ src/hooks/useFetchUsers.tsx
@@ -4,11 +4,12 @@ import { fetchUsers } from '@components/users/userSlice';
 import { AppDispatch, RootState } from '@/store';
 
 export const useFetchUsers = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch<AppDispatch>();
   const { error } = useSelector((state: RootState) => state.users);
 
   useEffect(() => {
+    setLoading(true);
     dispatch(fetchUsers()).then((action) => {
       if (fetchUsers.fulfilled.match(action)) {
         setLoading(false);
