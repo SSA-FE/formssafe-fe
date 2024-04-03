a74774952625f6a6c65ea662257d3d678242ddd9
kwakseongjae <gkffhdnls13@gmail.com>
2024-04-03 10:42:46 +0900

    chore: Myspace에서 user 데이터를 불러오는 코드 삭제

--- src/pages/Myspace.tsx
+++ src/pages/Myspace.tsx
@@ -1,11 +1,6 @@
 import Toolbar from '@/components/Myspace/Toolbar';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/store';
 
 const Myspace = () => {
-  const { user } = useSelector((state: RootState) => state.user);
-
-  console.log(user);
   return (
     <>
       <Toolbar />
