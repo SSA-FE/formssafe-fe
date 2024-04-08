5b0e3812687095bec20ae1183926eebaf6c3dbcd
박수진 <one_pst@naver.com>
2024-04-08 17:46:02 +0900

    feat: 질문리스트 질문 업데이트 기능

--- src/components/Myspace/questionBlockList/questionBlockListSlice.ts
+++ src/components/Myspace/questionBlockList/questionBlockListSlice.ts
@@ -1,13 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { ReactNode } from 'react';
-import { questionType } from '../questionBlockForm/QuestionBlockForm';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { option, questionType } from '../questionBlock/QuestionBlock';
 
 type questionBlock = {
   id: string;
+  type: questionType;
   title: string;
   description?: string;
-  children: ReactNode;
-  questionType: questionType;
+  options?: option[];
+  isRequired: boolean;
+  isPrivacy: boolean;
 };
 
 interface QuestionBlockList {
@@ -17,11 +18,29 @@ interface QuestionBlockList {
 const initialState: QuestionBlockList = {
   questionList: [
     {
-      id: 'randomUUID',
-      title: 'test',
-      description: 'test',
-      questionType: 'single',
-      children: '',
+      id: crypto.randomUUID(),
+      type: 'single',
+      title: '1반임',
+      description: '1반은 즐거워',
+      options: [
+        { id: crypto.randomUUID(), value: '11111' },
+        { id: crypto.randomUUID(), value: '22222' },
+        { id: crypto.randomUUID(), value: '3333' },
+      ],
+      isRequired: true,
+      isPrivacy: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      type: 'checkbox',
+      title: '2반임',
+      description: '2반은 즐거워',
+      options: [
+        { id: crypto.randomUUID(), value: '4444' },
+        { id: crypto.randomUUID(), value: '55555' },
+      ],
+      isRequired: true,
+      isPrivacy: false,
     },
   ],
 };
@@ -31,11 +50,19 @@ export const questionBlockListSlice = createSlice({
   initialState,
   reducers: {
     addQuestion: () => {},
+    updateQuestion: (state, action: PayloadAction<questionBlock>) => {
+      const index = state.questionList.findIndex(
+        (question) => question.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.questionList[index] = action.payload;
+      }
+    },
     removeQuestion: () => {},
     reorderQuestion: () => {},
   },
 });
 
-export const { addQuestion, removeQuestion, reorderQuestion } =
+export const { addQuestion, updateQuestion, removeQuestion, reorderQuestion } =
   questionBlockListSlice.actions;
 export default questionBlockListSlice.reducer;
