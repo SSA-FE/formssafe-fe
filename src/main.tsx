8a6ddb7571d0c3b0fcc787deb12e4bb395d91a55
kwakseongjae <gkffhdnls13@gmail.com>
2024-04-09 01:21:51 +0900

    chore: msw 설치 및 기본세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { Provider } from 'react-redux';
 import { store } from '@store/index';
 import Router from '@router/index';
 import './index.css';
+import { worker } from './mocks/browser';
+
+if (import.meta.env.VITE_APP_NODE_ENV === 'development') {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
