7a8e1a4eeee1cee00e253e5e76a601c8d284c484
kwakseongjae <gkffhdnls13@gmail.com>
2024-04-16 17:03:55 +0900

    feat: activityApi 작업 진행

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,14 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import questionBlockListReducer from '@/components/Myspace/questionBlockList/questionBlockListSlice';
 import { userApi } from '@api/userApi';
+import { activityApi } from '@/api/activityApi';
 
 export const store = configureStore({
   reducer: {
     questionBlockList: questionBlockListReducer,
     [userApi.reducerPath]: userApi.reducer,
+    [activityApi.reducerPath]: activityApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(userApi.middleware);
+    return getDefaultMiddleware().concat(
+      userApi.middleware,
+      activityApi.middleware
+    );
   },
 });
 
