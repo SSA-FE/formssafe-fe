881225c2da62f467ecec021166dea112e923d278
kwakseongjae <gkffhdnls13@gmail.com>
2024-04-18 16:37:25 +0900

    feat: 새 설문 만들기 버튼 기능 추가, 탭 이동시 resetInput dispatch

--- src/components/Myspace/MyspacePanel.tsx
+++ src/components/Myspace/MyspacePanel.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { resetInput } from './toolbar/toolbarInputSlice';
+import { useDispatch } from 'react-redux';
 
 interface MyspacePanelProps {
   setSurveyStatus: (value: string) => void;
@@ -6,18 +9,23 @@ interface MyspacePanelProps {
 
 const MyspacePanel = ({ setSurveyStatus }: MyspacePanelProps) => {
   const [activeButton, setActiveButton] = useState('보관함');
+  const dispatch = useDispatch();
 
   return (
     <div className="flex flex-col px-8 items-start space-y-2 border-b-[1px] bg-white">
-      <button className="box-border px-5 py-2 text-sm text-white font-bold rounded bg-neutral-800">
+      <Link
+        to="/editor"
+        className="box-border px-5 py-2 text-sm font-bold text-white bg-blue-400 rounded"
+      >
         새 설문 만들기
-      </button>
-      <div className="flex bg-transparent border-none space-x-4 font-bold">
+      </Link>
+      <div className="flex space-x-4 font-bold bg-transparent border-none">
         <button
           className={`px-6 py-2 ${activeButton === '보관함' ? 'border-b-2 border-neutral-800 ' : 'text-neutral-400 border-none'}`}
           onClick={() => {
             setActiveButton('보관함');
             setSurveyStatus('mySurveys');
+            dispatch(resetInput());
           }}
         >
           보관함
@@ -27,6 +35,7 @@ const MyspacePanel = ({ setSurveyStatus }: MyspacePanelProps) => {
           onClick={() => {
             setActiveButton('참여한 설문');
             setSurveyStatus('participatedSurveys');
+            dispatch(resetInput());
           }}
         >
           참여한 설문
