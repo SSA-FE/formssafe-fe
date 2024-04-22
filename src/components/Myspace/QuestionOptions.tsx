bf291e9ef544c1b5f751c39c813e9f29cf131e22
hanna kang <hanna0175@gmail.com>
2024-04-22 18:03:29 +0900

    feat: 블럭 생성 구현

--- src/components/Myspace/QuestionOptions.tsx
+++ src/components/Myspace/QuestionOptions.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { questionType } from '@components/Myspace/questionBlock/QuestionBlock';
 import SingleIcon from '@/assets/icons/option-single-icon.svg?react';
 import CheckboxIcon from '@/assets/icons/option-checkbox-icon.svg?react';
 import DropdownIcon from '@/assets/icons/option-dropdown-icon.svg?react';
@@ -5,27 +8,38 @@ import TextIcon from '@/assets/icons/option-text-icon.svg?react';
 import SentenceIcon from '@/assets/icons/option-sentence-icon.svg?react';
 
 const QuestionOptions = () => {
+  const dispatch = useDispatch();
+  const [questionTypeIcons] = useState<{ [key in questionType]: JSX.Element }>({
+    single: <SingleIcon />,
+    checkbox: <CheckboxIcon />,
+    dropdown: <DropdownIcon />,
+    text: <TextIcon />,
+    long: <SentenceIcon />,
+    short: <SentenceIcon />,
+  });
+
+  const addQuestion = (type: questionType) => {
+    const newQuestion = {
+      //   id: uuidv4(),
+      type,
+      isRequired: true,
+      isPrivacy: false,
+    };
+    dispatch({ type: 'questionBlockList/addQuestion', payload: newQuestion });
+  };
+
   return (
     <div className="flex flex-col w-full px-4 pt-4 pb-8 border-b-4 gap-xs border-slate-50">
-      <h2 className="text-xs font-bold text-slate-500">새 블록</h2>
+      <h2 className="text-xs font-bold text-slate-500">새 블록</h2>{' '}
       <div className="flex">
-        <button>
-          <SingleIcon />
-        </button>
-        <button>
-          <CheckboxIcon />
-        </button>
-        <button>
-          <DropdownIcon />
-        </button>
-        <button>
-          <TextIcon />
-        </button>
-        <button>
-          <SentenceIcon />
-        </button>
+        {Object.keys(questionTypeIcons).map((type) => (
+          <button key={type} onClick={() => addQuestion(type as questionType)}>
+            {questionTypeIcons[type as questionType]}
+          </button>
+        ))}
       </div>
     </div>
   );
 };
+
 export default QuestionOptions;
