f3fc1cb3fb832e8ab396f2c7999fa1c95645e8e5
hanna kang <hanna0175@gmail.com>
2024-04-22 16:39:30 +0900

    feat: 토글 버튼 구현

--- src/components/Myspace/FormOptionBar.tsx
+++ src/components/Myspace/FormOptionBar.tsx
@@ -1,8 +1,8 @@
 const FormOptionBar = () => {
   return (
-    <div className="w-[19rem] ml-auto h-[calc(100vh-8rem)] border border-neutral-200 bg-white rounded flex flex-col content-center self-stretch divide-y">
+    <div className="w-[19rem] ml-auto h-[calc(100vh-8rem)] border border-neutral-200 bg-white rounded flex flex-col content-center self-stretch">
       {/* 질문 유형 */}
-      <div className="flex flex-col w-full px-4 pt-3 pb-4 gap-xs">
+      <div className="flex flex-col w-full px-4 pt-3 pb-4 border-b-4 gap-xs border-slate-50">
         <h2 className="text-neutral-800">질문 유형</h2>
         <div className="flex flex-col gap-md">
           <p className="text-xs text-orange-400">
@@ -22,26 +22,34 @@ const FormOptionBar = () => {
           </div>
         </div>
       </div>
-      {/* 파일 선택 */}
-      <div className="flex items-center w-full px-4 pt-3 pb-4 text-xs gap-sm text-neutral-500">
-        <button className="bg-neutral-200 px-4 py-1.5 rounded-lg">
-          파일 선택하기
-        </button>
-        <span>질문 이미지 설정하기</span>
+      <div className="flex flex-col w-full">
+        {/* 개인정보 */}
+        <label className="flex items-center justify-between h-12 p-4 cursor-pointer">
+          <span className="text-xs font-bold text-blue-300">
+            개인정보
+            <span className="ml-1 text-orange-400">&#9432;</span>
+          </span>
+          <div>
+            <input type="checkbox" value="" className="sr-only peer" />
+            <div className="toggle-btn"></div>
+          </div>
+        </label>
+        {/* 필수응답 */}
+        <label className="flex items-center justify-between h-12 p-4 cursor-pointer">
+          <span className="text-xs font-bold text-neutral-400">
+            필수응답
+            <span className="ml-1 text-orange-400">&#9432;</span>
+          </span>
+          <div>
+            <input type="checkbox" value="" className="sr-only peer" />
+            <div className="toggle-btn"></div>
+          </div>
+        </label>
       </div>
-      {/* 플레이스홀더 */}
-      <div className="flex flex-col w-full px-4 pt-3 pb-4 gap-xs">
-        <h2 className="text-neutral-800">플레이스홀더</h2>
-        <div className="flex flex-col gap-md">
-          <p className="text-xs text-orange-400">
-            &#9432; 질문에 기본적으로 나타날 문자입니다.
-          </p>
-          <textarea className="w-full h-16 p-2 text-xs border rounded-lg resize-none outline-neutral-500 border-neutral-300 bg-neutral-100 text-neutral-400" />
-        </div>
-      </div>
+
       {/* 삭제하기 */}
-      <div className="flex flex-col w-full px-4 pt-3 pb-4 gap-xs">
-        <button className="w-full text-sm text-red-500 bg-neutral-200 h-7">
+      <div className="flex flex-col w-full px-4 pt-3 pb-4 border-t gap-xs border-slate-50">
+        <button className="w-full text-sm font-bold text-red-500 bg-neutral-100 h-7">
           삭제하기
         </button>
       </div>
