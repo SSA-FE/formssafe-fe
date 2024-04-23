3fe66779865c1943d03032522dda3a29a9df9cb7
kwakseongjae <gkffhdnls13@gmail.com>
2024-04-23 13:34:09 +0900

    fix: vercel 오류 해결

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,10 @@ export const BASE_URL: string = import.meta.env.VITE_APP_BASE_URL;
 export const GOOGLE_AUTH_URL: string = import.meta.env.VITE_APP_GOOGLE_AUTH_URL;
 
 export const API = {
-  AUTH: `/v1/auth`,
   USERS: `/v1/users`,
   FORM: `/v1/forms`,
   RESULT: `/v1/forms`,
   ACTIVITY: `/v1/activity`,
-  SUBMISSION: `/v1/forms`,
   FILES: `/v1/files`,
   VIEW: `/v1/view`,
   SUBMISSION: `/v1/forms`,
