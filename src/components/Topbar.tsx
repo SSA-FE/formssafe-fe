3404ba806f30940ffe2b4e4f6685d7d8590f15c0
kwakseongjae <gkffhdnls13@gmail.com>
2024-04-26 14:33:11 +0900

    design: 알람버튼, 프로필 버튼 호버, 클릭시 백그라운드 효과 부여

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -167,13 +167,13 @@ const Topbar = () => {
         <div className="relative">
           <div className="flex px-3 py-2 my-1 h-14 w-[124px] items-center justify-between">
             <button
-              className="flex items-center justify-center w-10 h-10 bg-transparent rounded-full cursor-pointer hover:bg-gray-100 focus:bg-gray-100"
+              className={`flex items-center justify-center w-10 h-10 bg-transparent rounded-full cursor-pointer hover:bg-gray-100 ${alarmModalOpen ? 'focus:bg-gray-200' : ''}`}
               onClick={handleAlarmButtonClick}
             >
               <InactiveAlarmIcon width="20" height="20" />
             </button>
             <button
-              className="flex items-center justify-center w-10 h-10 rounded-full cursor-pointer hover:bg-gray-100 focus:bg-gray-100"
+              className={`flex items-center justify-center w-10 h-10 rounded-full cursor-pointer hover:bg-gray-100 ${profileModalOpen ? 'focus:bg-gray-200' : ''}`}
               onClick={handleProfileButtonClick}
             >
               <img
