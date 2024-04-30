db1a7c353fccd872d6bd6f9af6eeb52a5cfe8f28
KimYuKyung <kyg0954@naver.com>
2024-04-30 19:15:22 +0900

    feat: 통계 페이지에 질문 블록 추가

--- src/pages/Stat.tsx
+++ src/pages/Stat.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Item from '@/components/Statistic/itemBlock/Item';
+import MultipleChoiceBlock from '@/components/Statistic/itemBlock/MutipleChoiceBlock';
+import EssayQuestionBlock from '@/components/Statistic/itemBlock/EssayQuestionBlock';
 import RemoteForm from '@/components/Statistic/remoteForm/RemoteForm';
+import { dummy } from '@/components/Statistic/itemBlock/dummy';
 
+// type -> multipleChoice, longAns, shortAns, checkBox, dropDown
 function Stat() {
   return (
     <>
-      {/* height : test */}
       <div className="flex h-[calc(200vh-6rem)]">
         {/* 사이드바 */}
         <div className="w-[19.5rem]">
@@ -11,8 +16,18 @@ function Stat() {
         </div>
 
         {/* 설문지 */}
-        <div className="flex-1 flex items-center justify-center bg-blue-50">
-          설문지
+        <div className="flex flex-col items-start justify-start flex-1 h-auto gap-24 pt-4">
+          {dummy.map((item: any, index: number) => (
+            <div>
+              <Item key={index} data={item} idx={index + 1}>
+                {item.type === 'longAns' || item.type === 'shortAns' ? (
+                  <EssayQuestionBlock data={item.options} />
+                ) : (
+                  <MultipleChoiceBlock data={item.options} />
+                )}
+              </Item>
+            </div>
+          ))}
         </div>
       </div>
     </>
