02390a12d7ff77c05e54c9b03683a0c8279f1cae
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-02 12:05:11 +0900

    refactor: topbar 컴포넌트 모달 분리 및 최적화 작업

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Topbar from '@/components/Topbar';
+import Topbar from '@/components/topbar/Topbar';
 import { Suspense } from 'react';
 import Loading from './Loading';
 
