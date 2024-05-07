effeb4b25cae65073887b0b820e7827efb1ccde8
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-07 15:55:37 +0900

    chore: EditIcon fill 색상값 변경

--- src/assets/icons/EditIcon.tsx
+++ src/assets/icons/EditIcon.tsx
@@ -7,17 +7,17 @@ const SvgEditIcon = (props: SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      stroke="#F1F5F9"
+      stroke="#1F2328"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth={1.5}
+      strokeWidth={1}
       d="M10 8v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h2"
     />
     <path
-      stroke="#F1F5F9"
+      stroke="#1F2328"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth={1.5}
+      strokeWidth={1}
       d="M6.25 7.9 11 3.1 8.9 1 4.15 5.75 4 8z"
     />
   </svg>
