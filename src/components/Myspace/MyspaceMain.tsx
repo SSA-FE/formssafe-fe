3cd75cdd9ea4e507a0fbe4c115bbe7dceafa9075
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-07 10:24:55 +0900

    style: FormList -> formList 카멜케이스로 변경

--- src/components/Myspace/MyspaceMain.tsx
+++ src/components/Myspace/MyspaceMain.tsx
@@ -10,6 +10,7 @@ import { RootState } from '@/store';
 import Loading from '@components/Loading';
 import { resetInput } from './toolbar/toolbarInputSlice';
 import { useAppDispatch } from '@/hooks/useAppDispatch';
+import MyspaceSidebar from './MyspaceSidebar';
 
 interface MyspaceMainProps {
   surveyStatus: string;
@@ -19,7 +20,7 @@ interface MyspaceMainProps {
 const MyspaceMain = ({ surveyStatus, setSurveyStatus }: MyspaceMainProps) => {
   const [activeTap, setActiveTap] = useState('등록한 설문');
   const dispatch = useAppDispatch();
-  const [FormList, setFormList] = useState<Form[] | null>(null);
+  const [formList, setFormList] = useState<Form[] | null>(null);
   const toolbarInput = useSelector((state: RootState) => state.toolbarInput);
   const registeredSurveysQuery = useFetchRegisteredSurveysQuery(toolbarInput);
   const participatedSurveysQuery =
@@ -46,37 +47,42 @@ const MyspaceMain = ({ surveyStatus, setSurveyStatus }: MyspaceMainProps) => {
   if (isLoading) return <Loading />;
 
   return (
-    <>
-      <div className="flex pl-8 space-x-4 text-sm font-bold bg-transparent bg-white border-b border-slate-200">
-        <button
-          className={`px-6 py-2 whitespace-nowrap ${activeTap === '등록한 설문' ? 'text-blue-400 border-b-2 border-blue-400 ' : 'text-neutral-400 border-b-2 border-neutral-400'}`}
-          onClick={() => {
-            setActiveTap('등록한 설문');
-            setSurveyStatus('mySurveys');
-            dispatch(resetInput());
-          }}
-        >
-          등록한 설문
-        </button>
-        <button
-          className={`px-6 py-2 whitespace-nowrap ${activeTap === '참여한 설문' ? 'text-blue-400 border-b-2 border-blue-400 ' : 'text-neutral-400 border-b-2 border-neutral-400'}`}
-          onClick={() => {
-            setActiveTap('참여한 설문');
-            setSurveyStatus('participatedSurveys');
-            dispatch(resetInput());
-          }}
-        >
-          참여한 설문
-        </button>
-      </div>
-      <div className="w-full min-h-screen p-4 bg-slate-50">
-        <div className="flex flex-wrap gap-4">
-          {FormList &&
-            Array.isArray(FormList) &&
-            FormList.map((form: Form) => <FormCard key={form.id} {...form} />)}
+    <div className="flex">
+      <MyspaceSidebar />
+      <div className="w-full">
+        <div className="flex pl-8 space-x-4 text-sm font-bold bg-transparent bg-white border-b-[2px] border-t-[2px] border-slate-100">
+          <button
+            className={`px-6 py-2 whitespace-nowrap ${activeTap === '등록한 설문' ? 'text-blue-400 border-b-2 border-blue-400 ' : 'text-neutral-400 border-b-2 border-neutral-400'}`}
+            onClick={() => {
+              setActiveTap('등록한 설문');
+              setSurveyStatus('mySurveys');
+              dispatch(resetInput());
+            }}
+          >
+            등록한 설문
+          </button>
+          <button
+            className={`px-6 py-2 whitespace-nowrap ${activeTap === '참여한 설문' ? 'text-blue-400 border-b-2 border-blue-400 ' : 'text-neutral-400 border-b-2 border-neutral-400'}`}
+            onClick={() => {
+              setActiveTap('참여한 설문');
+              setSurveyStatus('participatedSurveys');
+              dispatch(resetInput());
+            }}
+          >
+            참여한 설문
+          </button>
+        </div>
+        <div className="w-full min-h-screen p-4 bg-slate-50">
+          <div className="flex flex-wrap gap-4">
+            {formList &&
+              Array.isArray(formList) &&
+              formList.map((form: Form) => (
+                <FormCard key={form.id} {...form} />
+              ))}
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
