140dbd66d21e55c08fb22062198f08c14b617e54
coolfin <sorlti6952@gmail.com>
2024-05-07 13:45:23 +0900

    FEAT: smooth scroll 구현

--- src/components/Statistic/remoteForm/Question.tsx
+++ src/components/Statistic/remoteForm/Question.tsx
@@ -3,11 +3,18 @@ interface QuestionProps {
   number: number;
 }
 export default function Question(props: QuestionProps) {
+  const scrollSmooth = () => {
+    const el = document.getElementById('_content' + props.number.toString());
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
-    <a href={'#_content' + props.number.toString()} className="scroll-smooth">
-      <div className="w-[14rem] flex items-center h-10 px-4 text-xs border-b cursor-pointer shrink-0 border-slate-100 text-neutral-400 truncate hover:bg-blue-100">
-        {props.number + 1 + '. ' + props.question}
-      </div>
-    </a>
+    <button
+      onClick={scrollSmooth}
+      className="w-[14rem] flex items-center h-10 px-4 text-xs border-b cursor-pointer shrink-0 border-slate-100 text-neutral-400 truncate hover:bg-blue-100"
+    >
+      {props.number + 1 + '. ' + props.question}
+    </button>
   );
 }
