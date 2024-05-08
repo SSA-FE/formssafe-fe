b5134ef4aa1f11210298b205d59e52707cc67b46
coolfin <sorlti6952@gmail.com>
2024-05-08 16:42:43 +0900

    FEAT: 개별통계 사이드 컨테이너 기능 구현

--- src/components/Statistic/userResultForm/UserResultForm.tsx
+++ src/components/Statistic/userResultForm/UserResultForm.tsx
@@ -2,29 +2,48 @@ import { ChangeEvent, useState } from 'react';
 import User from './User';
 
 import searchIcon from '@assets/icons/search-icon.svg';
+import { answerDummy } from '../dummy';
 
-const DUMMY_USER_DATA: number[] = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-];
-const UserResultForm = () => {
-  const [userData, setUserData] = useState<number[]>(DUMMY_USER_DATA);
+interface UserResultFormProps {
+  setUserId: (userId: number) => void;
+}
+
+interface responseType {
+  responseId: number;
+  user: {
+    id: number;
+    nickname: string;
+  };
+  responses: {
+    questionId: number;
+    content: string | string[] | number;
+  }[];
+  responsedAt: string;
+}
+
+const UserResultForm = (props: UserResultFormProps) => {
+  const [userData, setUserData] = useState<responseType[]>(
+    answerDummy.totalResponses
+  );
   const handleSearchUser = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     if (e.target.value.trim() === '') {
-      setUserData(DUMMY_USER_DATA);
+      setUserData(answerDummy.totalResponses);
       return;
     }
-    const searchUserData = DUMMY_USER_DATA.filter((val: number) =>
-      val.toString().includes(e.target.value)
+    const searchUserData = answerDummy.totalResponses.filter(
+      (answer: responseType) => answer.user.nickname.includes(e.target.value)
     );
     setUserData(searchUserData);
   };
   return (
-    <div className="flex flex-1 flex-col h-screen sticky top-0 bg-slate-100 text-xs">
+    <div className="sticky top-0 flex flex-col flex-1 h-screen text-xs bg-slate-100">
       <div className="flex flex-col gap-2 h-[5.375rem] pt-2 pb-4">
         {/* StatusBar */}
         <div className="flex items-center justify-center h-6">
-          <h1 className="font-bold text-slate-400">총 40명의 설문자</h1>
+          <h1 className="font-bold text-slate-400">
+            총 {answerDummy.responseCnt}명의 설문자
+          </h1>
         </div>
 
         {/* 총 질문 개수 */}
@@ -43,8 +62,12 @@ const UserResultForm = () => {
 
       {/* 개별 질문리스트 */}
       <div className="flex flex-col items-start flex-1 overflow-y-scroll">
-        {userData.map((val: number, index: number) => (
-          <User key={'_survey_user' + index} name={val.toString()} />
+        {userData.map((answer: responseType, index: number) => (
+          <User
+            key={'_survey_user' + index}
+            user={answer.user}
+            setUserId={props.setUserId}
+          />
         ))}
       </div>
     </div>
--- src/pages/StatDetail.tsx
+++ src/pages/StatDetail.tsx
@@ -1,16 +1,18 @@
 import PersonalResult from '@/components/Statistic/PersonalResult';
 import UserResultForm from '@/components/Statistic/userResultForm/UserResultForm';
+import { useState } from 'react';
 
 const StatDetail = () => {
+  const [userId, setUserId] = useState<number>(2);
   return (
     <div className="flex h-[calc(200vh-6rem)] ">
       {/* 사이드바 */}
       <div className="w-[19.5rem]">
-        <UserResultForm />
+        <UserResultForm setUserId={setUserId} />
       </div>
 
       <div className="flex items-start justify-start flex-1">
-        <PersonalResult userId={2} />
+        <PersonalResult userId={userId} />
       </div>
     </div>
   );
