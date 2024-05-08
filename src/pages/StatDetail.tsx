506222475136c3187d82f81994dedcafc634cf8d
KimYuKyung <kyg0954@naver.com>
2024-05-08 14:15:34 +0900

    feat: 개별통계에 개별응답 컴포넌트 추가

--- src/pages/StatDetail.tsx
+++ src/pages/StatDetail.tsx
@@ -1,16 +1,16 @@
+import PersonalResult from '@/components/Statistic/PersonalResult';
 import UserResultForm from '@/components/Statistic/userResultForm/UserResultForm';
 
 const StatDetail = () => {
   return (
-    <div className="flex h-[calc(200vh-6rem)]">
+    <div className="flex h-[calc(200vh-6rem)] ">
       {/* 사이드바 */}
       <div className="w-[19.5rem]">
         <UserResultForm />
       </div>
 
-      {/* 설문지 */}
-      <div className="flex-1 flex items-center justify-center bg-blue-50">
-        설문지
+      <div className="flex items-start justify-start flex-1">
+        <PersonalResult userId={2} />
       </div>
     </div>
   );
