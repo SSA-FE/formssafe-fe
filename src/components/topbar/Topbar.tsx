4355a58f39d33a627a256000558806465f31dddb
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-08 10:10:28 +0900

    feat: topbar page navigation기능 추가 및 페이지별 동적 UI 렌더링 기능 추가

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { User, useFetchUserQuery } from '@api/userApi';
 import { useAppDispatch } from '@/hooks/useAppDispatch';
@@ -9,13 +9,15 @@ import { useEffect, useRef, useState } from 'react';
 import AlarmModal from './AlarmModal';
 import ProfileModal from './ProfileModal';
 import { RootState } from '@/store';
+import { GOOGLE_AUTH_URL } from '@/config';
 
 const Topbar = () => {
-  const { data, refetch } = useFetchUserQuery();
+  const { data, isError, refetch } = useFetchUserQuery();
   const [alarmModalOpen, setAlarmModalOpen] = useState(false);
   const [profileModalOpen, setProfileModalOpen] = useState(false);
   const user: User = useSelector((state: RootState) => state.topbarSlice);
   const location = useLocation();
+  const navigate = useNavigate();
   const modalRef = useRef(null);
   const dispatch = useAppDispatch();
 
@@ -54,6 +56,14 @@ const Topbar = () => {
       !(modalRef.current as HTMLElement).contains(event.target as Node)
     ) {
       setModalOpen(false);
+    }
+  };
+
+  const handleLogin = () => {
+    if (isError) {
+      window.location.href = GOOGLE_AUTH_URL;
+    } else {
+      navigate('/board');
     }
   };
 
@@ -102,22 +112,24 @@ const Topbar = () => {
           <NavLink className="text-lg font-bold text-slate-600" to="/">
             <MainLogoIcon width="80" />
           </NavLink>
-          <div className="flex space-x-4 font-bold text-sm">
-            <NavLink to="/board">
-              <span
-                className={`${location.pathname === '/board' ? 'text-blue-500 hover:border-b-[1px] hover:border-blue-500' : 'text-slate-800 hover:border-b-[1px] hover:border-slate-800'}  py-0 leading-none inline-block`}
-              >
-                보드
-              </span>
-            </NavLink>
-            <NavLink to="/myspace">
-              <span
-                className={`${location.pathname === '/myspace' ? 'text-blue-500 hover:border-b-[1px] hover:border-blue-500' : 'text-slate-800 hover:border-b-[1px] hover:border-slate-800'}  py-0 leading-none inline-block`}
-              >
-                마이스페이스
-              </span>
-            </NavLink>
-          </div>
+          {location.pathname !== '/' && (
+            <div className="flex space-x-4 font-bold text-sm">
+              <NavLink to="/board">
+                <span
+                  className={`${location.pathname === '/board' ? 'text-blue-500 hover:border-b-[1px] hover:border-blue-500' : 'text-slate-800 hover:border-b-[1px] hover:border-slate-800'}  py-0 leading-none inline-block`}
+                >
+                  보드
+                </span>
+              </NavLink>
+              <NavLink to="/myspace">
+                <span
+                  className={`${location.pathname === '/myspace' ? 'text-blue-500 hover:border-b-[1px] hover:border-blue-500' : 'text-slate-800 hover:border-b-[1px] hover:border-slate-800'}  py-0 leading-none inline-block`}
+                >
+                  마이스페이스
+                </span>
+              </NavLink>
+            </div>
+          )}
         </div>
 
         {location.pathname !== '/' && user.imageUrl && (
@@ -141,6 +153,14 @@ const Topbar = () => {
               </button>
             </div>
           </div>
+        )}
+        {location.pathname === '/' && (
+          <button
+            className="shadow-md flex items-center w-auto px-5 py-2 space-x-2 text-xs font-bold text-white bg-blue-500 rounded-[38px] h-9 whitespace-nowrap"
+            onClick={handleLogin}
+          >
+            <p className="flex items-center p-0 m-0"> 시작하기</p>
+          </button>
         )}
       </nav>
     </>
