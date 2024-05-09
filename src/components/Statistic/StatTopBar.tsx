5e3dae9ab277a142bdabfb91bc4215f333b0778f
coolfin <sorlti6952@gmail.com>
2024-05-09 14:38:35 +0900

    FEAT: 출력하기 api 구현 -print 버튼 이미지 추가 -config result경로 변경

--- src/components/Statistic/StatTopBar.tsx
+++ src/components/Statistic/StatTopBar.tsx
@@ -1,12 +1,33 @@
 import statCheckIcon from '@/assets/icons/stat-check-icon.svg';
 import editIcon from '@/assets/icons/edit-square-icon.svg';
+import printIcon from '@/assets/icons/print-icon.svg';
 import { Link, useLocation } from 'react-router-dom';
+import { instance } from '@/api/axios';
+import { API } from '@/config';
 
 const StatTopBar = () => {
   const location = useLocation();
 
   const focusNav = 'border-b-2 border-black';
   const blurNav = 'text-slate-400';
+
+  const printForm = () => {
+    instance
+      .get(`${API.RESULT}/forms/42/download`, {
+        responseType: 'blob',
+      })
+      .then((res) => {
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', '설문결과_전체요약.xlsx');
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="flex items-center w-full h-12 px-8 py-2">
@@ -37,16 +58,19 @@ const StatTopBar = () => {
 
       {/* buttons */}
       <div className="w-[15.25rem] flex justify-between items-center gap-4 text-xs text-slate-50 font-bold">
-        <button className="w-[6.8125rem] h-9 rounded-full bg-neutral-400 drop-shadow">
-          출력하기
+        <button
+          onClick={printForm}
+          className="flex items-center px-4 py-2 rounded-full gap-x-2 bg-neutral-400 drop-shadow"
+        >
+          <img className="cover" src={printIcon} alt="print-icon" />
+          <span>출력하기</span>
         </button>
-        <button className="w-[7.4375rem] h-9 px-6 flex items-center gap-x-2 rounded-full bg-blue-400 drop-shadow">
-          <img className="w-[10px] h-[10px]" src={editIcon} alt="edit-icon" />
+        <button className="flex items-center px-4 py-2 bg-blue-400 rounded-full gap-x-2 drop-shadow">
+          <img className="cover" src={editIcon} alt="edit-icon" />
           <span>마감하기</span>
         </button>
       </div>
     </div>
   );
 };
-
 export default StatTopBar;
