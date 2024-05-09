21fca91489ba50b0ac996f6d8dc4c2067f789943
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-09 14:04:22 +0900

    chore: loading spinner width 값 변경

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from '@assets/icons';
 const Loading = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <Spinner width="40" />
+      <Spinner width="80" />
     </div>
   );
 };
