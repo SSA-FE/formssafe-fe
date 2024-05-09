5e7074ac77be199001472edbd0caf6d2402a80ca
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-09 14:05:08 +0900

    feat: Home 페이지 변경 및 Footer 디자인 작업

---
+++ src/components/Footer.tsx
@@ -0,0 +1,53 @@
+import { FooterLogoIcon } from '@/assets/icons';
+
+const Footer = () => {
+  return (
+    <div className="w-full h-footer bg-gradient-to-r from-slate-50 to-slate-100 pt-2 px-16 flex flex-col gap-8">
+      <div className="w-full h-12 flex items-center space-x-2 border-b border-slate-200">
+        <p className="text-sm font-bold text-slate-600">공지사항</p>
+        <div className="flex space-x-2">
+          <span className="bg-blue-500 rounded-xl text-white px-2 py-[1px] font-bold text-[10px] flex justify-center items-center">
+            MAIN
+          </span>
+          <p className="font-bold text-[10px] text-slate-600">
+            폼나는 싸패는 2024년 05월 13일에 정식 배포되었습니다.
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between text-xs text-slate-600">
+        <div className="flex space-x-7">
+          <div className="min-w-[150px]">
+            <FooterLogoIcon width="150" />
+          </div>
+          <p className="flex justify-center items-center">
+            이메일 ssafe.dev@gmail.com | 연락처 010-3107-1487
+            <br />
+            <br />
+            본 사이트의 콘텐츠는 저작권법의 보호를 받는 바 무단 전재, 복사, 배포
+            등을 금합니다.
+            <br />
+            Copyright © SSAFE 2024 All Rights Reserved.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <span className="text-sm font-bold text-slate-800">SNS</span>
+          <ul className="flex flex-col gap-2 text-xs text-slate-600">
+            <li>
+              <a href="https://kwakseongjae.notion.site/33246b4a66a64cde943d513ca774faf9?pvs=4">
+                Notion
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/SSA-FE">Github</a>
+            </li>
+            <li>
+              <a href="/">Figma</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
