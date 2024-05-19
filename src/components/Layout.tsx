72024a4775ed2c940949a73a19416a2e57208a59
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-20 03:31:45 +0900

    chore: protected router 로직 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import Topbar from '@/components/topbar/Topbar';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import Footer from './Footer';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useFetchUserQuery } from '@/api/userApi';
 
 export default function Layout({ hasBackground }: { hasBackground: boolean }) {
+  const { isError } = useFetchUserQuery();
   const location = useLocation();
+  const navigate = useNavigate();
   const path = location.pathname;
+
+  useEffect(() => {
+    if (path !== '/' && path !== '/join/' && isError) {
+      navigate('/', { replace: true });
+    }
+  }, [isError, path, navigate]);
 
   return (
     <Suspense>
