b652a4aa19642bfa7fa50ef47ba88eaf9982b2aa
kwakseongjae <gkffhdnls13@gmail.com>
2024-05-19 18:07:00 +0900

    feat: thumbnail이 없는 경우 기본 thumbnail 표시

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -3,6 +3,7 @@ import FormBody from '@/components/Form/FormBody';
 import FormHeader from '@/components/Form/FormHeader';
 import Loading from '@/components/Loading';
 import { useParams } from 'react-router-dom';
+import defaultImg from '@assets/images/dafaultImg.jpg';
 
 const Form = () => {
   const { id } = useParams<{ id: string }>();
@@ -11,12 +12,21 @@ const Form = () => {
   if (isLoading || !survey) return <Loading />;
 
   return (
-    <div className="bg-slate-100 min-h-screen">
-      <img
-        src={survey.image && survey.image[0]}
-        alt="thumbnail"
-        className="w-full h-24 object-cover"
-      />
+    <div className="min-h-screen bg-slate-100">
+      {survey.image && survey.image[0] ? (
+        <img
+          src={survey.image[0]}
+          alt="thumbnail"
+          className="object-cover w-full h-24"
+        />
+      ) : (
+        <img
+          src={defaultImg}
+          alt="thumbnail"
+          className="object-cover w-full h-24"
+        />
+      )}
+
       <div className="max-w-[832px] flex flex-col gap-2 mx-auto">
         <FormHeader formData={survey} />
         <FormBody surveyId={surveyId} questions={survey.contents} />
