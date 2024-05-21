49aa470aade075c897d4939d1cea68c7c9e71234
곽성재 <87296259+kwakseongjae@users.noreply.github.com>
2024-05-21 11:45:05 +0900

    design: 수정일 p요소 제거

--- src/components/Myspace/FormToolBar.tsx
+++ src/components/Myspace/FormToolBar.tsx
@@ -49,25 +49,20 @@ const FormToolBar = () => {
   };
 
   return (
-    <div className="bg-white flex w-full px-8 h-12 whitespace-nowrap justify-between items-start">
-      <div>
-        <p className="text-xs text-blue-400 px-4 py-1 bg-blue-50 border border-blue-200">
-          2024/05/05에 마지막으로 수정됨
-        </p>
-      </div>
+    <div className="flex items-start justify-end w-full h-12 px-8 bg-white whitespace-nowrap">
       <div className="flex gap-md">
-        <button className="border px-8 py-2 text-xs font-bold text-slate-400 rounded-full h-9 hover:bg-slate-50">
+        <button className="px-8 py-2 text-xs font-bold border rounded-full text-slate-400 h-9 hover:bg-slate-50">
           미리보기
         </button>
         <button
           onClick={() => handleFormCreation(true)}
-          className="border px-8 py-2 text-xs font-bold text-slate-400 rounded-full h-9 hover:bg-slate-50"
+          className="px-8 py-2 text-xs font-bold border rounded-full text-slate-400 h-9 hover:bg-slate-50"
         >
           임시저장
         </button>
         <button
           onClick={() => handleFormCreation(false)}
-          className="shadow-md px-8 py-2 text-xs font-bold text-white bg-blue-500 rounded-full h-9 hover:bg-blue-400"
+          className="px-8 py-2 text-xs font-bold text-white bg-blue-500 rounded-full shadow-md h-9 hover:bg-blue-400"
         >
           발행하기
         </button>
